623820e596b47dd20b47d5ec82ed1d2dc9aefa44
Tashisha <jovichnatasha@gmail.com>
2022-04-14 13:11:27 +0100

    added response?

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function getTemp(response) {
 }
 
 function getForecast (coordinates) {
-    console.log(coordinates);
+    
 
     let apiKey = "1bac14ffc7b0daec09f7ccca1d8eaa84";
     let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
@@ -94,7 +94,7 @@ function getForecast (coordinates) {
     axios.get(apiURL).then(displayForecast);
 }
 
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
