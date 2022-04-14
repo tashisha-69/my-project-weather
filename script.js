00e892fc72538a1f18c23f696a98e50ba4a8003d
Tashisha <jovichnatasha@gmail.com>
2022-04-14 13:16:07 +0100

    check console log and checking if this works

--- script.js
+++ script.js
@@ -82,11 +82,11 @@ function getTemp(response) {
     let h4 = document.querySelector("h4");
     h4.innerHTML = humidity
 
-    getForecast(coordinates);
+    displayForecast(response);
 }
 
 function getForecast (coordinates) {
-    
+    console.log(coordinates);
 
     let apiKey = "1bac14ffc7b0daec09f7ccca1d8eaa84";
     let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
@@ -95,6 +95,7 @@ function getForecast (coordinates) {
 }
 
 function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
