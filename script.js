4cb3a95238fe4fa8564ca88762e6cce4fc5beb82
Tashisha <jovichnatasha@gmail.com>
2022-04-14 13:25:19 +0100

    find coordinates in console log

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function getTemp(response) {
     let h4 = document.querySelector("h4");
     h4.innerHTML = humidity
 
-    displayForecast(response);
+    
 }
 
 function getForecast (coordinates) {
@@ -91,7 +91,7 @@ function getForecast (coordinates) {
     let apiKey = "1bac14ffc7b0daec09f7ccca1d8eaa84";
     let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
 
-    axios.get(apiURL).then(displayForecast);
+    
 }
 
 function displayForecast(response) {
