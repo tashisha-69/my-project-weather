cf379cfb78fe1557fc5caa0a2667415646f124b2
Tashisha <jovichnatasha@gmail.com>
2022-04-14 13:03:00 +0100

    maybe working?

--- script.js
+++ script.js
@@ -81,6 +81,8 @@ function getTemp(response) {
     
     let h4 = document.querySelector("h4");
     h4.innerHTML = humidity
+
+    getForecast(coordinates);
 }
 
 function getForecast (coordinates) {
@@ -89,7 +91,7 @@ function getForecast (coordinates) {
     let apiKey = "1bac14ffc7b0daec09f7ccca1d8eaa84";
     let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
 
-    axios.get(getTemp).then(displayForecast);
+    axios.get(apiURL).then(displayForecast);
 }
 
 function displayForecast() {
